4bb2928e2ec1f39c99bc0b32cfe0f86e7f7068ee
Jeff Musa <jeffmusa@gmail.com>
2018-10-22 21:18:36 +0300

    neighbour/urls.py

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -12,7 +12,6 @@ urlpatterns=[
     url(r'^biz/', views.biz, name='biz'),
     url(r'^hood/(\d+)', views.hood, name='hood'),
     url(r'^post/', views.post, name='post'),
-    url(r'^post/', views.post, name='post'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
 ]
