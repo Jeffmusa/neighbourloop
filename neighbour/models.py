8694c8e4ba89801db6ff8462fd0e3616c3017d6e
Jeff Musa <jeffmusa@gmail.com>
2018-10-22 16:41:28 +0300

    neighbour/models.py

--- neighbour/models.py
+++ neighbour/models.py
@@ -9,24 +9,46 @@ class Neighbour(models.Model):
     image = models.ImageField(upload_to = 'images/',null=True)
     occupants = models.IntegerField(null=True)
     user = models.ForeignKey(User, null=True)
+    objects = models.Manager()
     # Admin Foreign key
+    def __str__(self):
+        return self.name
 
 
 # User class
 class Profile(models.Model):
     pro_photo = models.ImageField(upload_to = 'images/',null=True)
     name = models.CharField(max_length =30,null=True)
+    location = models.CharField(max_length =30,null=True)
     email = models.EmailField(max_length =50,null=True)
     neighbourhood = models.ForeignKey(Neighbour, null=True)
+    bio = models.CharField(max_length =150,null=True)
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile',null=True)
-
+    
+    def __str__(self):
+        return self.name
 
 class Business(models.Model):
     name = models.CharField(max_length =30,null=True)
+    description = models.CharField(max_length =130,null=True)
     email = models.EmailField(max_length =50,null=True)
     user = models.ForeignKey(User, null=True)
     neighbourhood = models.ForeignKey(Neighbour, null=True)
+ 
+    objects = models.Manager()
+    def __str__(self):
+        return self.name
 
+
+class Post(models.Model):
+    post = models.CharField(max_length =130,null=True)
+    user = models.ForeignKey(User, null=True)
+    neighbourhood = models.ForeignKey(Neighbour,related_name='post',null=True)
+
+    objects = models.Manager()
+ 
+    def __str__(self):
+        return self.name
 
 
 
